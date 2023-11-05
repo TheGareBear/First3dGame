7e986e25d305ff7fb034fc01f995a5385300f4ac
GVassar <51205052+TheGareBear@users.noreply.github.com>
2023-11-05 11:53:25 -0700

    Basic enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    public float speed;
+    public Vector3 moveOffset;
+    public Vector3 targetPos;
+    private Vector3 startPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
+        targetPos = startPos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if(transform.position == targetPos)
+        {
+            if(targetPos == startPos)
+            {
+                targetPos = startPos + moveOffset;
+            }
+            else
+            {
+                targetPos = startPos;
+            }
+        }
     }
 }
